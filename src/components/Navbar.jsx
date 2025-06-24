7eacc1c805e98d74281a12345d67c37c116172bb
Andres Jimenez <ajthecoder1@gmail.com>
2025-06-24 14:58:53 +0000

    starwars commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,60 @@
+
 import { Link } from "react-router-dom";
+import React from "react";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
 
+	const {store, dispatch} = useGlobalReducer();
+	const favorites = store.favorites;
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+			<div className="container-fluid">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img 
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/1920px-Star_wars2.svg.png" 
+					className="d-inline-block align-text-top" 
+					alt="star wars logo"
+					style={{ 
+						width: "7rem",
+						height: "4rem",
+					}}
+					/>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+				<div className="dropdown ms-auto">
+          <button
+            className="btn btn-outline-success dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Favorites ({favorites.length})
+          </button>
+
+          <ul className="dropdown-menu dropdown-menu-end">
+            {favorites.length === 0 ? (
+              <li className="dropdown-item text-muted">No favorites yet</li>
+            ) : (
+              favorites.map((name, index) => (
+                <li
+                  key={index}
+                  className="dropdown-item d-flex justify-content-between align-items-center"
+                >
+                  {name}
+                  <button
+                    className="btn btn-sm btn-outline-secondary ms-2"
+                    onClick={() =>
+                      dispatch({ type: "REMOVE_FAVORITE", payload: name })
+                    }
+                    title="Remove favorite"
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
 			</div>
 		</nav>
 	);
--- src/routes.jsx
+++ src/routes.jsx
@@ -7,8 +7,9 @@ import {
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
+import { CharacterInfo } from "./pages/CharacterInfo";
+import { PlanetInfo } from "./pages/PlanetsInfo"; 
+import { VehicleInfo } from "./pages/VehiclesInfo";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,8 +24,9 @@ export const router = createBrowserRouter(
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        <Route path="/demo" element={<Demo />} />
+        <Route path="/character-info/:uid" element={ <CharacterInfo />} />  
+        <Route path="/planet-info/:uid" element={ <PlanetInfo />} />
+        <Route path="/vehicle-info/:uid" element={ <VehicleInfo />} />
       </Route>
     )
 );
